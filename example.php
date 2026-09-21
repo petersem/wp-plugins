3f74d2f2768947f68ca13ed97becc56233062170
petersem <matthew.petersem.68@gmail.com>
2026-09-21 13:17:35 +1000

    testing auto versioning

--- example.php
+++ example.php
@@ -3,10 +3,11 @@
  * Plugin Name: 💀💀 Matt's Example Plugins 💀💀
  * Plugin URI: https://www.youtube.com/watch?v=xvFZjo5PgG0
  * Description: WordPress example plugins. One file to rule them all. 😛😛😛
- * Version: 1.1
+ * Version: 1.2.1
  * Author: Matt Petersen
- * Author URI: nesretep.net
- * Text Domain: example-plugin
+ * Author URI: https://github.com/petersem
+ * GitHub Plugin URI: https://github.com/petersem/wp-plugins
+ * GitHub Branch: main
  */
 
 // stop plugin from being executed outside of WP
