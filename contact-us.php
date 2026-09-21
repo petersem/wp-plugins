5c99a875f8bebbcb7101f854132602fc84350830
petersem <matthew.petersem.68@gmail.com>
2026-09-21 22:28:43 +1000

    te

--- contact-us.php
+++ contact-us.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Contact Form
 * Description: Custom contact form with full client-side + server-side validation, sticky values, and admin management.
-* Version: 1.2
+* Version: 1.3
 * Author: Matt
 */
 
