e8c73c5b4fdf93df24c5b2969621aae3a563a72e
petersem <matthew.petersem.68@gmail.com>
2026-09-21 22:07:32 +1000

    v i

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
  * Plugin Name: 💀💀 Matt's Example Plugins 💀💀
  * Plugin URI: https://www.youtube.com/watch?v=xvFZjo5PgG0
  * Description: WordPress example plugins. One file to rule them all. 😛😛😛
- * Version: 1.2.17
+ * Version: 1.2.18
  * Author: Matt Petersen
  * Author URI: https://github.com/petersem
  * GitHub Plugin URI: https://github.com/petersem/wp-plugins
