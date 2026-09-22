5c640c1380004a87a2d153baa6b5a583eb92b663
petersem <matthew.petersem.68@gmail.com>
2026-09-22 14:53:47 +1000

    test

--- weather-ticker.php
+++ weather-ticker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: weather ticker
  * Plugin URI: nesretep.net
- * Description: WordPress weather ticker plugin with Admin Custom Latitude & Longitude controls.
+ * Description: WWeather ticker plugin with Admin Custom Latitude & Longitude controls.
  * Version: 1.0.0
  * Author: Matt Petersen
  * Author URI: nesretep.net
